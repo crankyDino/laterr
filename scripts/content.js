eae1295e67cb512653dbdb0e67511bdab7c52e9c
skywizard <bamodise+git@protonmail.com>
2025-02-28 22:25:34 +0200

    testing functionality

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,7 @@
 (() => {
-    console.log("I'm content");
+
+    // console.log(document.getElementsByTagName("h1")[0].title);
+    console.log(document.getElementsByTagName("h1")[0].textContent);
+    // console.log(document.getElementsByClassName("title"));
+
 })()
--- scripts/workers/background.js
+++ scripts/workers/background.js
@@ -1,6 +1,14 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     console.log("I'm here in the background!");
-// });
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    console.log(tab.url);
+
+    // const queryParameters = tab.url.split("?")[1];
+    // const urlParameters = new URLSearchParams(queryParameters);
+
+    // chrome.tabs.sendMessage(tabId, {
+    //     type: "NEW",
+    //     videoId: urlParameters.get("v"),
+    // });
+});
 
 chrome.action.onClicked.addListener(() => {
     console.log("I'm here in the background actioning");
@@ -17,3 +25,4 @@ chrome.runtime.onInstalled.addListener(() => {
 //         sendResponse({ status: "Background script is active!" });
 //     }
 // });
+
