24f449b46dc20a5635ba31ab0a3c8ad4060082c5
skywizard <bamodise+git@protonmail.com>
2025-03-01 00:07:31 +0200

    Test: upstream/downstream communication

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,21 @@
 (() => {
-    const bookmark = document.getElementById("bookmark");
+    // const bookmark = document.getElementById("bookmark");
+
+
+
 
     bookmark.addEventListener("click", () => {
-        console.log("Ayo, i been clicked");
+        // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        //     console.log("Ayo, i been clicked");
+        //     if (tabs.length === 0) return;
+        //     chrome.tabs.sendMessage(tabs[0].id, { action: "updateContent", data: "lightblue" });
+        // });
+
+        chrome.storage.local.get("currentTab", (res) => {
+            console.log(res);
+        })
     });
-    
+
+
+
 })();
